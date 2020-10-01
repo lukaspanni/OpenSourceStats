810afae4901e484b40f6a4655d3c95f321909a8f
Lukas Panni <lukas.panni@outlook.de>
2020-10-01 15:41:14 +0200

    Add: AuthActivity

--- src/main/java/com/example/opensoucestats/MainActivity.java
+++ src/main/java/com/example/opensoucestats/MainActivity.java
@@ -1,29 +1,12 @@
 package com.example.opensoucestats;
 
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.apollographql.apollo.ApolloCall;
-import com.apollographql.apollo.ApolloClient;
-import com.apollographql.apollo.api.CustomTypeAdapter;
-import com.apollographql.apollo.api.CustomTypeValue;
-import com.apollographql.apollo.api.Error;
-import com.apollographql.apollo.api.Input;
-import com.apollographql.apollo.api.Response;
-import com.apollographql.apollo.exception.ApolloException;
-import com.example.opensourcestats.LoginQuery;
-import com.example.opensourcestats.UserContributionsQuery;
-import com.example.opensourcestats.type.CustomType;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,31 +15,37 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.apollographql.apollo.ApolloCall;
+import com.apollographql.apollo.ApolloClient;
+import com.apollographql.apollo.api.CustomTypeAdapter;
+import com.apollographql.apollo.api.CustomTypeValue;
+import com.apollographql.apollo.api.Error;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
+import com.example.opensoucestats.auth.AuthActivity;
+import com.example.opensoucestats.auth.AuthHandler;
+import com.example.opensourcestats.LoginQuery;
+import com.example.opensourcestats.UserContributionsQuery;
+import com.example.opensourcestats.type.CustomType;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import net.openid.appauth.AuthState;
 import net.openid.appauth.AuthorizationException;
-import net.openid.appauth.AuthorizationRequest;
 import net.openid.appauth.AuthorizationResponse;
 import net.openid.appauth.AuthorizationService;
-import net.openid.appauth.AuthorizationServiceConfiguration;
 import net.openid.appauth.ClientAuthentication;
-import net.openid.appauth.ClientSecretBasic;
 import net.openid.appauth.ClientSecretPost;
-import net.openid.appauth.ResponseTypeValues;
 import net.openid.appauth.TokenRequest;
 import net.openid.appauth.TokenResponse;
 
 import org.jetbrains.annotations.NotNull;
-import org.json.JSONException;
 
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -65,9 +54,6 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
 public class MainActivity extends AppCompatActivity {
-
-    private AuthorizationService authService;
-    private AuthState authState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,80 +68,16 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-
-        authState = this.readAuthState();
-        if (!authState.isAuthorized()) {
-            authenticate();
+        if(getIntent().getAction() == null){
+            return;
         }
-    }
-
-    private void authenticate() {
-        final String CLIENT_ID = getString(R.string.client_id);
-        final Uri REDIRECT_URI = Uri.parse("de.lukaspanni.oss://opensourcestats/auth");
-        AuthorizationServiceConfiguration serviceConfig = new AuthorizationServiceConfiguration(
-                Uri.parse("https://github.com/login/oauth/authorize"), // authorization endpoint
-                Uri.parse("https://github.com/login/oauth/access_token")); // token endpoint
-
-        authState = new AuthState(serviceConfig);
-
-        AuthorizationRequest.Builder requestBuilder = new AuthorizationRequest.Builder(serviceConfig, CLIENT_ID, ResponseTypeValues.CODE, REDIRECT_URI);
-        AuthorizationRequest request = requestBuilder.setScope("repo:status").build();
-
-        authService = new AuthorizationService(this);
-        Intent authIntent = authService.getAuthorizationRequestIntent(request);
-        startActivityForResult(authIntent, 42);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        final Context ctx = this;
-
-        if (requestCode == 42) {
-            final AuthorizationResponse resp = AuthorizationResponse.fromIntent(data);
-            AuthorizationException ex = AuthorizationException.fromIntent(data);
-            if (resp != null) {
-                ClientAuthentication clientAuth = new ClientSecretPost(getString(R.string.client_secret));
-                TokenRequest req = resp.createTokenExchangeRequest();
-                authService.performTokenRequest(req, clientAuth, new AuthorizationService.TokenResponseCallback() {
-                    @Override
-                    public void onTokenRequestCompleted(
-                            TokenResponse response, AuthorizationException ex) {
-
-                        authState.update(response, ex);
-                        if(authState.isAuthorized()){
-                            writeAuthState(authState);
-                        }
-                        if (response != null) {
-                            Toast.makeText(ctx, "SUCCESS", Toast.LENGTH_LONG).show();
-                            useToken();
-                        } else {
-                            Toast.makeText(ctx, ex.getLocalizedMessage(), Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
-            } else {
-                Toast.makeText(this, "FAILED - " + ex.getLocalizedMessage(), Toast.LENGTH_LONG).show();
-            }
+        //TODO: start with AuthActivity -> less activity switches
+        AuthHandler handler = AuthHandler.getInstance(this);
+        if(!handler.checkAuth()){
+            Intent authIntent = new Intent(this, AuthActivity.class);
+            startActivity(authIntent);
         }
-    }
 
-    private void writeAuthState(@NonNull AuthState authState){
-        SharedPreferences authPreferences = getSharedPreferences("auth", MODE_PRIVATE);
-        authPreferences.edit().putString("authState", authState.jsonSerializeString()).apply();
-    }
-
-    private AuthState readAuthState(){
-        SharedPreferences authPreferences = getSharedPreferences("auth", MODE_PRIVATE);
-        String stateString = authPreferences.getString("authState", null);
-        if(stateString != null) {
-            try {
-                return AuthState.jsonDeserialize(stateString);
-            } catch (JSONException e) {
-                Log.e("Error Loading State", e.getLocalizedMessage());
-            }
-        }
-        return new AuthState();
     }
 
     class AuthInterceptor implements Interceptor {
@@ -165,7 +87,6 @@ public class MainActivity extends AppCompatActivity {
             this.token = token;
         }
 
-
         @Override
         public okhttp3.Response intercept(Chain chain) throws IOException {
             Request req = chain.request().newBuilder().addHeader("Authorization", this.token).build();
@@ -173,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void useToken() {
+    /* private void useToken() {
         authState.performActionWithFreshTokens(authService, new AuthState.AuthStateAction() {
             @Override
             public void execute(@Nullable String accessToken, @Nullable String idToken, @Nullable AuthorizationException ex) {
@@ -268,5 +189,5 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
-    }
+    } */
 }
---
+++ src/main/java/com/example/opensoucestats/auth/AuthActivity.java
@@ -0,0 +1,68 @@
+package com.example.opensoucestats.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.opensoucestats.MainActivity;
+import com.example.opensoucestats.R;
+
+import net.openid.appauth.AuthorizationException;
+import net.openid.appauth.AuthorizationResponse;
+import net.openid.appauth.AuthorizationService;
+import net.openid.appauth.ClientAuthentication;
+import net.openid.appauth.ClientSecretPost;
+import net.openid.appauth.TokenRequest;
+import net.openid.appauth.TokenResponse;
+
+public class AuthActivity extends AppCompatActivity {
+
+    private AuthHandler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        handler = AuthHandler.getInstance(this);
+        setContentView(R.layout.activity_auth);
+        Button auth_button = (Button) findViewById(R.id.auth_button);
+        auth_button.setOnClickListener(v -> {
+            handler.authenticate();
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        final Context ctx = this;
+
+        if (requestCode == AuthHandler.REQUEST_CODE) {
+            final AuthorizationResponse resp = AuthorizationResponse.fromIntent(data);
+            AuthorizationException ex = AuthorizationException.fromIntent(data);
+            if (resp != null) {
+                ClientAuthentication clientAuth = new ClientSecretPost(getString(R.string.client_secret));
+                TokenRequest req = resp.createTokenExchangeRequest();
+                handler.getAuthService().performTokenRequest(req, clientAuth, (response, ex1) -> {
+
+                    handler.getAuthState().update(response, ex1);
+                    if (response != null) {
+                        Toast.makeText(ctx, getString(R.string.auth_success_text), Toast.LENGTH_LONG).show();
+                        Log.i("AUTH", "User authenticated");
+                        Intent toMainIntent = new Intent(this, MainActivity.class);
+                        startActivity(toMainIntent);
+                    } else {
+                        Toast.makeText(ctx, getString(R.string.auth_failed_text), Toast.LENGTH_LONG).show();
+                        Log.e("AUTH", ex1.getLocalizedMessage());
+                    }
+                });
+            } else {
+                Toast.makeText(ctx, getString(R.string.auth_failed_text), Toast.LENGTH_LONG).show();
+                Log.e("AUTH",  ex.getLocalizedMessage());
+            }
+        }
+    }
+}
