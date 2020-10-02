d250f49602208558b08db2bd7f5f95f00fd41aad
Lukas Panni <lukas.panni@outlook.de>
2020-10-02 12:47:25 +0200

    Start UI dev, simple display

--- src/main/java/com/example/opensoucestats/ui/dashboard/DashboardFragment.java
+++ src/main/java/com/example/opensoucestats/ui/dashboard/DashboardFragment.java
@@ -1,27 +1,16 @@
 package com.example.opensoucestats.ui.dashboard;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.opensoucestats.R;
-import com.example.opensoucestats.auth.AuthHandler;
-import com.example.opensoucestats.client.ClientDataCallback;
-import com.example.opensoucestats.client.GHClient;
-import com.example.opensoucestats.client.ResponseData;
-import com.example.opensoucestats.client.UserContributionsResponse;
-
-import java.util.Date;
 
 public class DashboardFragment extends Fragment {
 
@@ -32,34 +21,12 @@ public class DashboardFragment extends Fragment {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        final TextView overview = root.findViewById(R.id.text_overview);
+        final TextView repos = root.findViewById(R.id.text_repos);
+        dashboardViewModel.getOverview().observe(getViewLifecycleOwner(), s -> overview.setText(s));
+        dashboardViewModel.getRepositories().observe(getViewLifecycleOwner(), s -> repos.setText(s));
 
-        AuthHandler handler = AuthHandler.getInstance(getActivity());
-        if (handler.checkAuth()) {
-            GHClient client = new GHClient(handler);
-            Date start = new Date(System.currentTimeMillis() - (7 * 1000 * 60 * 60 * 24));
-            Date end = new Date();
-            client.loadData(start, end, new ClientDataCallback() {
-                @Override
-                public void callback(ResponseData data) {
-                    UserContributionsResponse responseData = (UserContributionsResponse) data;
-                    if (data == null) return;
-                    getActivity().runOnUiThread(() -> {
-                        StringBuilder outBuilder = new StringBuilder("Current Week:\n");
-                        outBuilder.append("Commits: " + responseData.getCommits() + "\n");
-                        outBuilder.append("Issues: " + responseData.getIssues() + "\n");
-                        outBuilder.append("PullRequests: " + responseData.getPullRequests() + "\n");
-                        textView.setText(outBuilder.toString());
-                    });
-                }
-            });
-        }
+        dashboardViewModel.loadData(getActivity());
 
         return root;
     }
--- src/main/java/com/example/opensoucestats/ui/dashboard/DashboardViewModel.java
+++ src/main/java/com/example/opensoucestats/ui/dashboard/DashboardViewModel.java
@@ -1,19 +1,66 @@
 package com.example.opensoucestats.ui.dashboard;
 
+import android.app.Activity;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.opensoucestats.auth.AuthHandler;
+import com.example.opensoucestats.client.ClientDataCallback;
+import com.example.opensoucestats.client.GHClient;
+import com.example.opensoucestats.client.ResponseData;
+import com.example.opensoucestats.client.UserContributionsResponse;
+
+import java.util.Date;
+
 public class DashboardViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private MutableLiveData<String> overviewText;
+    private MutableLiveData<String> repositoriesText;
+    private GHClient client;
+    private AuthHandler handler;
 
     public DashboardViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        overviewText = new MutableLiveData<>();
+        repositoriesText = new MutableLiveData<>();
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<String> getOverview() {
+        return overviewText;
+    }
+
+    public LiveData<String> getRepositories() {
+        return repositoriesText;
+    }
+
+    public void loadData(Activity activity) {
+        if (handler == null) {
+            handler = AuthHandler.getInstance(activity);
+        }
+        if (handler.checkAuth()) {
+            if (client == null) {
+                client = new GHClient(handler);
+            }
+            Date start = new Date(System.currentTimeMillis() - (7 * 1000 * 60 * 60 * 24));
+            Date end = new Date();
+            client.loadData(start, end, new ClientDataCallback() {
+                @Override
+                public void callback(ResponseData data) {
+                    UserContributionsResponse responseData = (UserContributionsResponse) data;
+                    if (data == null) return;
+                    StringBuilder overviewOut = new StringBuilder("Current Week:\n");
+                    overviewOut.append("Commits: ").append(responseData.getCommits()).append("\n");
+                    overviewOut.append("Issues: ").append(responseData.getIssues()).append("\n");
+                    overviewOut.append("PullRequests: ").append(responseData.getPullRequests()).append("\n");
+                    overviewText.postValue(overviewOut.toString());
+                    StringBuilder repositoriesOut = new StringBuilder("Repositories:\n");
+                    repositoriesOut.append("Commits: ").append(responseData.getCommitRepositories()).append("\n");
+                    //repositoriesOut.append("Issues: ").append(responseData.getIssueRepositories()).append("\n");
+                    //repositoriesOut.append("PullRequests: ").append(responseData.getPullRequestRepositories()).append("\n");
+                    repositoriesText.postValue(repositoriesOut.toString());
+                }
+            });
+        }
     }
 }
