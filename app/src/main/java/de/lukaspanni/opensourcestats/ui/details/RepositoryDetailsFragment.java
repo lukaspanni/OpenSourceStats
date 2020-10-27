14690c36e91eefdb5ea029bc70ef2333336bc446
Lukas Panni <lukas.panni@outlook.de>
2020-10-27 16:41:49 +0100

    Fix: RepositoryDetails back button

--- app/src/main/java/de/lukaspanni/opensourcestats/ui/details/RepositoryDetailsFragment.java
+++ app/src/main/java/de/lukaspanni/opensourcestats/ui/details/RepositoryDetailsFragment.java
@@ -2,16 +2,20 @@ package de.lukaspanni.opensourcestats.ui.details;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.lukaspanni.opensourcestats.R;
+
+import java.util.Objects;
 
 import de.lukaspanni.opensourcestats.MainActivity;
 import de.lukaspanni.opensourcestats.ui.custom_elements.card.RepositoryDetailsCard;
@@ -20,6 +24,21 @@ import de.lukaspanni.opensourcestats.ui.custom_elements.card.RepositoryDetailsCa
 public class RepositoryDetailsFragment extends Fragment {
 
     private RepositoryDetailsViewModel viewModel;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            Objects.requireNonNull(getActivity()).onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
