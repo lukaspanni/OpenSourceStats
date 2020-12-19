3ee8ba0902f7274772dc2340b48d399d427451b8
Lukas Panni <lukas.panni@outlook.de>
2020-12-19 11:40:14 +0100

    Reduce duplicate Code in RepositoryLists ExtractMethod getObservableList and move observe call to base

--- app/src/main/java/de/lukaspanni/opensourcestats/ui/details/PullRequestReviewRepositoryList.java
+++ app/src/main/java/de/lukaspanni/opensourcestats/ui/details/PullRequestReviewRepositoryList.java
@@ -4,23 +4,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+
+import androidx.lifecycle.LiveData;
 
 import com.lukaspanni.opensourcestats.R;
 
-import java.util.Objects;
+import java.util.List;
 
 public class PullRequestReviewRepositoryList extends RepositoryListFragment {
 
-    public PullRequestReviewRepositoryList(){
+    public PullRequestReviewRepositoryList() {
         super(R.layout.fragment_pull_request_review_repository_list);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = super.onCreateView(inflater, container, savedInstanceState);
-        detailsViewModel.getPullRequestReviewRepositories().observe(getViewLifecycleOwner(), strings -> setListAdapter(new ArrayAdapter<>(Objects.requireNonNull(getContext()), R.layout.repository_list_item, R.id.repo_list_item, strings.toArray())));
-        return v;
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    protected LiveData<List<String>> getObservableList() {
+        return detailsViewModel.getPullRequestReviewRepositories();
     }
 
     @Override
--- app/src/main/java/de/lukaspanni/opensourcestats/ui/details/RepositoryListFragment.java
+++ app/src/main/java/de/lukaspanni/opensourcestats/ui/details/RepositoryListFragment.java
@@ -1,24 +1,27 @@
 package de.lukaspanni.opensourcestats.ui.details;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.LayoutRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.ListFragment;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.Navigation;
 
+import com.lukaspanni.opensourcestats.R;
+
+
+import java.util.List;
 import java.util.Objects;
 
-import de.lukaspanni.opensourcestats.MainActivity;
 import de.lukaspanni.opensourcestats.util.TimeSpan;
 
 
@@ -59,8 +62,11 @@ public abstract class RepositoryListFragment extends ListFragment {
 
         assert timeSpan != null;
         detailsViewModel.loadData(timeSpan);
+        getObservableList().observe(getViewLifecycleOwner(), strings -> setListAdapter(new ArrayAdapter<>(Objects.requireNonNull(getContext()), R.layout.repository_list_item, R.id.repo_list_item, strings.toArray())));
         return view;
     }
+
+    protected abstract LiveData<List<String>> getObservableList();
 
     @Override
     public void onListItemClick(@NonNull ListView l, @NonNull View v, int position, long id) {
@@ -71,6 +77,7 @@ public abstract class RepositoryListFragment extends ListFragment {
         Navigation.findNavController(v).navigate(getNavigationAction(), b);
 
     }
+
 
     protected abstract int getNavigationAction();
 }
